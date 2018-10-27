3bdf435b0586ab940c140da6865c994ce0c357fa
ly2011 <1518550424@qq.com>
2018-10-28 07:05:44 +0800

    feat: add egg-mongoose

--- config/config.default.js
+++ config/config.default.js
@@ -1,13 +1,38 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = exports = {};
+  const config = (exports = {});
+
+  config.name = '饿了么管理后台';
+  config.description = '基于Egg + Mongoose的饿了么管理后台';
+
+  // debug 为 true 时，用于本地调试
+  config.debug = true;
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1540679595310_294';
 
+  config.session_secret = 'egg_elm';
+
   // add your config here
   config.middleware = [];
+
+  /**
+   * @see http://mongodb.github.io/node-mongodb-native/2.2/api/Db.html#createCollection
+   */
+  config.mongoose = {
+    client: {
+      url: process.env.EGG_MONGODB_URL || 'mongodb://127.0.0.1:27017/egg_elm',
+      options: {
+        server: { poolSize: 20 },
+        reconnectTries: 10,
+        reconnectInterval: 500,
+      },
+    },
+  };
+
+  config.default_page = 1;
+  config.default_limit = 20;
 
   return config;
 };
